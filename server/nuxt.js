d1cf55c1845ffc8cfe20d8968d60594c36864695
Alban Mouton <alban.mouton@gmail.com>
2022-04-25 15:07:45 +0200

    chore: no need to import nuxt.config.js in dec

--- server/nuxt.js
+++ server/nuxt.js
@@ -1,12 +1,13 @@
-const nuxtConfig = require('../nuxt.config.js')
-
 module.exports = async () => {
   if (process.env.NODE_ENV === 'development') {
     // in dev mode the nuxt dev server is already running, we re-expose it
     return require('http-proxy-middleware')({ target: 'http://localhost:3039' })
   } else {
     const { Nuxt } = require('nuxt-start')
+    const nuxtConfig = require('../nuxt.config.js')
+
     // Prepare nuxt for rendering and serving UI
+    nuxtConfig.dev = false
     const nuxt = new Nuxt(nuxtConfig)
     return async (req, res, next) => {
       // re-apply the prefix that was removed by an optional reverse proxy
